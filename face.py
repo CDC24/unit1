930c62dba5b88e268c7b5a37c8fdd343b622ce0e
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-17 08:24:25 -0500

    Updated from Brython Server: 1/17/2018 8:24:23 AM

--- face.py
+++ face.py
@@ -1,1 +1,10 @@
+#Caleb Callaway
+#1/17/18
+#face.py- drawing a face
 
+print("         XXXXX")
+print("        X     X")
+print("      ((  0 0  ))")
+print("        |  V  |")
+print("        | === |")
+print("         -----")
