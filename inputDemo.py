cd7db84e9fa2ebc0495c682755735d646b23a367
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-17 08:17:11 -0500

    Updated from Brython Server: 1/17/2018 8:17:09 AM

--- inputDemo.py
+++ inputDemo.py
@@ -1,1 +1,9 @@
+#Caleb Callaway
+#1/17/18
+#inputDemo.py- how to use input
 
+name = input("What is your name? ")
+print("Hello ", name)
+age = int(input("How old are you? "))
+#the int makes the value be seen as a number
+print("Congratulations, ", age+1,"-year-old!")
