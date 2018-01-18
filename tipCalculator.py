ddc1d341dcf68bd134fa39cc02ee6989de222c4f
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-18 08:30:03 -0500

    Updated from Brython Server: 1/18/2018 8:30:03 AM

--- tipCalculator.py
+++ tipCalculator.py
@@ -1,1 +1,10 @@
+#Caleb Callaway
+#1/18/18
+#tipCalculator.py- calculates and adds a tip
 
+price = int(input("What is the price of your meal in dollars? $"))
+percentTip = int(input("what is the percentage you plan to tip? "))
+tip = (percentTip/100)
+total = ((tip*price)+price)
+print("you should tip $",tip*price,".")
+print("your total is $",(total),".")
