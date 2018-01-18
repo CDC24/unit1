16c37652a48a9c8a2f69c3a950a03da38b95fb0c
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-18 08:17:30 -0500

    Updated from Brython Server: 1/18/2018 8:17:30 AM

--- age.py
+++ age.py
@@ -1,3 +1,10 @@
 #Caleb Callaway
-#1/17/18
-#age.py
+#1/18/18
+#age.py -count letters and numbers
+
+name = input("What is your name (first and last only)? ")
+age = int(input("how old are you? "))
+name1, name2= name.split( )
+print("You have",len(name1),"letters in your first name.")
+print("You have",len(name2),"letters in your first name.")
+print("Next year you will be",(age)+1,"years old.")
