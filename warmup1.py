1a1a356b99d4f80b0cc76813b424a45b309d4477
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-19 08:24:46 -0500

    Updated from Brython Server: 1/19/2018 8:24:45 AM

--- warmup1.py
+++ warmup1.py
@@ -1,1 +1,8 @@
+#Caleb Callaway
+#1/19/18
+#warmpu1.py-calculates area based on input base and height
 
+
+base = float(input("Base of triangle: "))
+height = float(input("height of triangle: "))
+print ("The area is", base*height/2,".")
