ba18c3e1c271652f597cee4893c1fee137678f4c
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-19 08:43:24 -0500

    Updated from Brython Server: 1/19/2018 8:43:23 AM

--- stringAnalysis.py
+++ stringAnalysis.py
@@ -1,1 +1,8 @@
+#Caleb Callaway
+#1/19/18
+#stringAnalysis.py- report words and characters of a sentence
 
+
+sentence = input("Type a sentence. ")
+character = input("what character do you want to search for? ")
+print (character.count(sentence))
