9ba96f1aa2383c6f4884a4e5130082b2f0731326
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-19 08:34:26 -0500

    Updated from Brython Server: 1/19/2018 8:34:26 AM

--- slope.py
+++ slope.py
@@ -1,1 +1,10 @@
+#Caleb Callaway
+#1/19/18
+#slope.py- calculate sope between 2 points
 
+X1 = float(input("what is the x coorinate of point A? "))
+Y1 = float(input("what is the y coorinate of point A? "))
+X2 = float(input("what is the x coorinate of point B? "))
+Y2 = float(input("what is the y coorinate of point B? "))
+
+print("The slope is", (X1-X2)/(Y1-Y2))
