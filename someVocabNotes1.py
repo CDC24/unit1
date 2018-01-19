f8e5fe5eee56014bc12442c2d47c6e3f1dc55a72
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-19 08:20:46 -0500

    Update and rename someNotes.py to someVocabNotes1.py

---
+++ someVocabNotes1.py
@@ -0,0 +1,13 @@
+#data type- category for a piece of info, such as:
+#   int-> a number, with no decimal
+#   float-> number with decimal
+#   string- anything in quotes, letters or digits
+#   Boolean- True or False
+
+
+# variable- name for a piece of data
+#   your variable name should tell you what it represents, it can be long
+#   variable names must start with a letter but can have numbers, but no spaces
+#   varible names go on the left during assignment (size = 43)
+
+
