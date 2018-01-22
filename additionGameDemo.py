846d4fc058f56dc1f3693cb4492fb95b577f3665
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-22 08:17:33 -0500

    Update additionGameDemo.py

--- additionGameDemo.py
+++ additionGameDemo.py
@@ -1,1 +1,15 @@
+#Caleb Callaway
+#1/22/18
+#additionGameDemo.py- how to use random numbers
 
+from random import randint
+#randint is random integer
+
+num1 = randint(-10,10)
+#parentheses give upper and lower limit
+num2 = randint(-10,10)
+ans = int(input(str(num1)+"+"+str(num2)+"= "))
+#connect strings with pluses instead of commas to put together
+
+print (ans == num1 + num2)
+# == checks truth
