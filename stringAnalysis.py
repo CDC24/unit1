24006cefa3e758101cd523bd51bc11a863d88eec
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-22 08:32:17 -0500

    Updated from Brython Server: 1/22/2018 8:32:16 AM

--- stringAnalysis.py
+++ stringAnalysis.py
@@ -5,4 +5,7 @@
 
 sentence = input("Type a sentence. ")
 character = input("what character do you want to search for? ")
-print (character.count(sentence))
+print ("There are",sentence.count(character))"'s in your sentence.")
+
+word = input("what word do you want to search for? ")
+print ("Your sentence has",sentence.count(word)),"of the word",word)
