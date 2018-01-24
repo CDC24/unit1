87b5630073b257469476fc57067774515c3ffda5
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-24 08:39:26 -0500

    Updated from Brython Server: 1/24/2018 8:39:25 AM

--- change.py
+++ change.py
@@ -1,1 +1,18 @@
+#Caleb Callaway
+#1/24/17
+#change.py- converts an input to numbers of smaller parts
 
+
+allMoney = float(input("Enter an amount of money.  $"))
+
+quarters= (allMoney//0.25)
+print (quarters,"quarters")
+
+dimes = ((allMoney-(quarters*0.25))//0.10)
+print (dimes,"dimes")
+
+nickels = ((allMoney-(quarters*0.25)-(dimes*0.10))//0.05)
+print (nickels,"nickels")
+
+pennies = ((allMoney-(quarters*0.25)-(dimes*0.10)-(nickels*0.05))//0.01)
+print (pennies, "pennies")
