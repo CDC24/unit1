060ab372331f75f9d84c3842efe00aaa84dd2fe3
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-24 08:22:20 -0500

    Updated from Brython Server: 1/24/2018 8:22:19 AM

--- eggs.py
+++ eggs.py
@@ -1,1 +1,9 @@
+#Caleb Callaway
+#1/24/17
+#eggs.py- checks relationships between two variables
 
+eggs = int(input("How many eggs do you have? "))
+cartons = int(input("How many egg cartons do you have? "))
+
+True = (12*cartons >= eggs)
+False = (12*carons < eggs)
