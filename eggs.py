86ceffc76e7f2d943e5ce9ba7bacb2fcc4e2b09b
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-24 08:24:46 -0500

    Updated from Brython Server: 1/24/2018 8:24:44 AM

--- eggs.py
+++ eggs.py
@@ -5,5 +5,4 @@
 eggs = int(input("How many eggs do you have? "))
 cartons = int(input("How many egg cartons do you have? "))
 
-True = (12*cartons >= eggs)
-False = (12*carons < eggs)
+print (12*cartons >= eggs)
