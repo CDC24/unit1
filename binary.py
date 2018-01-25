53678259cfe2b2eee9a8ae8852c684fa797dcfa7
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-25 11:06:13 -0500

    Updated from Brython Server: 1/25/2018 11:06:10 AM

--- binary.py
+++ binary.py
@@ -2,8 +2,8 @@
 #1/25/18
 #binary.py- converts 8 digit binary numbers to base 10
 
-num = int(input("Enter an 8-digit binary number: "))
+num = (input("Enter an 8-digit binary number with spaces between each digit: "))
 
-a,b,c,d,e,f,g,h = num.split()
+a, b, c, d, e, f, g, h = num.split()
 
-print (a)
+print (h*10**0 + g*10**1 + f*10**2 + e*10**3 + d*10**4 + c*10**5 + b*10**6 + a*10**7)
