21f8e217a26b0231e1823497cee9c9f3e5e72249
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-25 08:28:59 -0500

    Updated from Brython Server: 1/25/2018 8:28:59 AM

--- isItATriangle.py
+++ isItATriangle.py
@@ -1,1 +1,11 @@
+#Caleb Callaway
+#1/25/18
+#isItATriangle.py - determines if 3 numbers could be the sides of a triangle
 
+A = float(input("Enter side A: "))
+B = float(input("Enter side B: "))
+C = float(input("Enter side C: "))
+
+min(A+B) == C
+min(B+C)== A
+min(C+A)== B
