e3453874db8fc639225412872b0107a7fb3923b0
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-01-25 08:11:29 -0500

    Updated from Brython Server: 1/25/2018 8:11:28 AM

--- warnUp2.py
+++ warnUp2.py
@@ -1,1 +1,9 @@
+#Caleb Callaway
+#1/24/17
+#warmUp2- displays 'lucky number of the day'
 
+from random import randint
+
+name = input("What is your name? ")
+
+print ("Your lucky number is", randint(0,200))
