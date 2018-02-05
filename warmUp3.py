34c14c2f034fb03360a3689677365539bdfd1237
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-02-05 08:10:00 -0500

    Update warmUp3.py

--- warmUp3.py
+++ warmUp3.py
@@ -1,3 +1,13 @@
 #Caleb Callaway
 #2/5/18
 #warmUp3.py- a buzz game about divisibility
+
+
+num = float(input("Enter a number: ")
+seven = ("seven")
+
+if num%7 == 0:
+    print ("buzz")
+elif num.count(seven) > 0:
+    print ("buzz")
+else: print (num)
