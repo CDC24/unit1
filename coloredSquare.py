9392afb3d648a97053965e135554e3d0d10621e7
CDC24 <35492804+CDC24@users.noreply.github.com>
2018-02-05 08:38:09 -0500

    Updated from Brython Server: 2/5/2018 8:38:09 AM

--- coloredSquare.py
+++ coloredSquare.py
@@ -3,10 +3,36 @@
 #coloredSquare.py- uses sprites to display colored squares
 
 
+
+
 from ggame import *
 red = Color(0xff0000, 1)
 line = LineStyle(3,red)
-rectangle = RectangleAsset(100,100,line,red)
-Sprite(rectangle)
-myApp = App()
-myApp.run()
+rectangleRed = RectangleAsset(100,100,line,red)
+
+yellow = Color(0xffff00, 1)
+line = LineStyle(3,yellow)
+rectangleYellow = RectangleAsset(100,100,line,yellow)
+
+from random import randint
+
+num = randint (1,4)
+
+
+if num == 1:
+    Sprite(rectangleRed)
+    myApp = App()
+    myApp.run()
+
+elif num == 2:
+    Sprite(rectangleYellow)
+    myApp = App()
+    myApp.run()
+
+elif num == 3:
+    print ("Matzo balls")
+
+elif num == 4:
+     print ("Matzo balls")
+
+
